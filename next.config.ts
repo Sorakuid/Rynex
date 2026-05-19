78c1dd59ca1cb18c49785dc0efab9323e7c058bd
Riu <echo.soraku@gmail.com>
2026-05-19 08:26:00 +0700

    fix: bug 505 vercel

--- next.config.ts
+++ next.config.ts
@@ -9,14 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: "/api/auth/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
